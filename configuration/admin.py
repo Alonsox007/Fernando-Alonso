cb4eacb2582a2f6404987673c80fae9f1c0500ab
Alonsox007 <alonso_14_geminis@hotmail.com>
2021-07-12 16:03:23 -0500

    Update admin.py

--- configuration/admin.py
+++ configuration/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from configuration.models import Client
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'domain_url']
